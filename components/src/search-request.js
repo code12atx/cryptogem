79fad011d8de40cea7b9f166863b4809f1a8df42
trent.earl <trent.earl@malauzai.com>
2014-12-25 09:23:18 -0600

    fixes from prod

--- components/src/search-request.js
+++ components/src/search-request.js
@@ -1,14 +1,14 @@
 module.exports = {
     search: function(string) {
         return this.props.request(
-            'http://localhost:9200/cryptogem/locator/_search',
+            'http://search.cryptogem.com/cryptogem/locator/_search',
             { q: 'title:' + string + '*' }
         );
     },
 
     searchGet: function(string) {
         return this.props.request(
-            'http://localhost:9200/cryptogem/locator/' + string,
+            'http://search.cryptogem.com/cryptogem/locator/' + string,
             {}
         );
     },
--- s3toelasticsearch.rb
+++ s3toelasticsearch.rb
@@ -17,40 +17,54 @@ def es_index client, key, body
 end
 
 queue = AWS::SQS.new.queues.named 'crytogem-locator'
-queue_message = queue.receive_message
+while true
+  queue_message = queue.receive_message
 
-exit unless queue_message
+  if queue_message
 
-puts 'found message'
+    puts 'found message'
 
-puts queue_message.body
+    puts queue_message.body
 
-message = JSON.parse queue_message.body
+    message = JSON.parse queue_message.body
 
-def s3
-  AWS::S3.new.buckets['cryptogem.locator']
+    def s3
+      AWS::S3.new.buckets['cryptogem.locator']
+    end
+
+    def get_from_s3 key
+      s3.objects[key].read
+    end
+
+    records = message['Records'].map { |record|
+      begin
+        content = get_from_s3 record['s3']['object']['key']
+        metadata = content.split('$$').first
+      rescue AWS::S3::Errors::AccessDenied
+        queue_message.delete
+        metadata = ''
+      end
+
+      if !metadata.empty?
+        metadata = JSON.parse metadata
+
+        {
+          'time' => record['eventTime'],
+          'ip' => record['requestParameters']['sourceIPAddress'],
+          'key' =>  record['s3']['object']['key'],
+          'eTag' =>  record['s3']['object']['eTag'],
+        }.merge metadata
+      else
+        false
+      end
+    }.select{ |gem| gem }.each { |gem|
+      es_index es, gem['key'], gem
+    }
+
+    queue_message.delete
+    puts records
+  end
+
+  sleep 1
 end
-
-def get_from_s3 key
-  s3.objects[key].read
-end
-
-records = message['Records'].map { |record|
-  content = get_from_s3 record['s3']['object']['key']
-  metadata = JSON.parse content.split('$$').first
-
-  puts content
-  {
-    'time' => record['eventTime'],
-    'ip' => record['requestParameters']['sourceIPAddress'],
-    'key' =>  record['s3']['object']['key'],
-    'eTag' =>  record['s3']['object']['eTag'],
-  }.merge metadata
-}.each { |gem|
-  es_index es, gem['key'], gem
-}
-
-queue_message.delete
-puts records
-
 
